d655438d70ae537eb5b16eb7dfdf74b14f7cf789
Muhammed Thasneem P Android <muhammed.thasneem@yahoo.com>
2017-08-15 13:43:30 +0530

    Enabled Data binding

--- AndroidDatabinding/app/src/main/java/app/demo/muhammed/androiddatabinding/MainActivity.java
+++ AndroidDatabinding/app/src/main/java/app/demo/muhammed/androiddatabinding/MainActivity.java
@@ -1,13 +1,21 @@
 package app.demo.muhammed.androiddatabinding;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import app.demo.muhammed.androiddatabinding.databinding.MainActivityBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private MainActivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.main_activity);
+        binding.addressTextView.setText("Muhammed Thasneem");
+
     }
 }
